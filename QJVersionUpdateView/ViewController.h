3ff950392ed1815ac2ae9b349af4384f69147ae0
qiji <Justin.Qi@zhan.com>
2016-03-08 17:33:11 +0800

    add project

---
+++ QJVersionUpdateView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QJVersionUpdateView
+//
+//  Created by Justin on 16/3/8.
+//  Copyright © 2016年 Justin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # QJVersionUpdateView
 when you release the new version,  QJVersionUpdateView will show users to notice update app
+
+when you use demo, you should change youre own APPID, it will work.
